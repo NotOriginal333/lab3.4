c11fe55f4edcbac9e81993051b2d38968a787d72
NotOriginal333 <m.naumovych2@gmail.com>
2023-09-23 15:45:30 +0300

    Add project files.

---
+++ lab3.4/lab3.4.cpp
@@ -0,0 +1,27 @@
+﻿// Lab3.4.cpp 
+// < Наумович Роман > 
+// Лабораторна робота № 3.4 
+// Розгалуження, задане плоскою фігурою. 
+// Варіант 18 
+
+#include <iostream> 
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x;  // вхідний аргумент 
+    double y;  // вхідний параметр 
+    double R;  // вхідний параметр
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    cout << "R = "; cin >> R;
+
+    // розгалуження в повній формі 
+    if (((y<=x)&&(y>=0)&&(R==sqrt(x*x+y*y)))||((y>=x)&&(R==sqrt(x*x+y*y)&&y<=0)))
+    cout << "yes" << endl;
+    else
+        cout << "no" << endl;
+        cin.get();
+        return 0;
+}
